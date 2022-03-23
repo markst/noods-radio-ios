e3a57b6125b98b7053c0170978fb9edab6890e06
Mark Turner <git@markst.co.uk>
2022-03-23 12:34:23 +0000

    Skips loading main application if running for previews.

--- Application/AppDelegate.swift
+++ Application/AppDelegate.swift
@@ -1,22 +1,28 @@
 import UIKitPlus
 
 class AppDelegate: BaseApp {
+  
   @AppBuilder override var body: AppBuilderContent {
-    Lifecycle.didFinishLaunching {
-      
-    }.willResignActive {
-      
-    }.willEnterForeground {
-      
-    }
+    Lifecycle
+      .didFinishLaunching {
+        
+      }
+      .willResignActive {
+        
+      }
+      .willEnterForeground {
+        
+      }
+    
     MainScene { .main }
     .mainScreen {
-      NavigationController(
-        UIViewController()
-          .background(.red))
+      // Avoid loading `ShowsListViewController` is rendering previews:
+      if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+        return UIViewController()
+      }
+      return NavigationController(ShowsListViewController(viewModel: ShowsListViewModel()))
         .style(.transparent)
         .hideNavigationBar()
     }
-    
   }
 }
