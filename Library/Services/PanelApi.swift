5de19105d10ef25885f4b5e40852482bd1a3a92d
Mark Turner <git@markst.co.uk>
2022-03-23 18:14:20 +0000

    Additional `PanelApi` endpoints.

--- Library/Services/PanelApi.swift
+++ Library/Services/PanelApi.swift
@@ -2,7 +2,15 @@ import Foundation
 import Moya
 
 enum PanelApi {
+  case home
   case shows
+  case collections
+  case donate
+  case info
+  case latest
+  case guests
+  case residents
+  case featured(page: Int)
   case show(id: String)
 }
 
@@ -10,33 +18,49 @@ extension PanelApi: TargetType {
   var baseURL: URL {
     URL(string: "https://panel.noodsradio.com")!
   }
-
+  
   var path: String {
     switch self {
     case .shows:
       return "shows.json"
+    case .home:
+      return "home.json"
+    case .collections:
+      return "collectionsjson"
+    case .donate:
+      return "donate.json"
+    case .info:
+      return "info.json"
+    case .latest:
+      return "latest.json"
+    case .guests:
+      return "guests.json"
+    case .residents:
+      return "residents.json"
+    case .featured(let page):
+      return "shows/featured.json/page:\(page)"
     case .show(let id):
       return "\(id).json"
     }
   }
-
+  
   var method: Moya.Method {
     switch self {
-    case .shows, .show:
+    case .home, .shows, .collections, .donate, .info, .latest, .guests, .residents, .featured, .show:
       return .get
     }
   }
-
+  
   var task: Task {
     switch self {
-    case .shows, .show:
+    case .home, .shows, .collections, .donate, .info, .latest, .guests, .residents, .featured, .show:
       return .requestPlain
     }
   }
-
+  
   var headers: [String : String]? {
     switch self {
-    case .shows, .show:
+    case .home, .shows, .collections, .donate, .info, .latest, .guests, .residents, .featured, .show:
       return ["Content-type": "application/json"]
     }
   }
